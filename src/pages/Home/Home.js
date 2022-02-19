04b88939fc39d740ea7d23383492afcfca35af72
Ícaro Almeida <progalmeidaregis@outlook.com>
2022-02-19 20:00:29 -0300

    estilização da pagina home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FiLink } from 'react-icons/fi';
+import Logo from '../../assets/Logo.png';
 import './style.css';
 
 export default function Home() {
   return (
     <div className="container-home">
-      <div className="logo-home">Página Home</div>
+      <div className="logo-home">
+        <img className="image-home" src={Logo} alt="Logo Link" />
+        <h1 className="title-home">EncurtaLinks</h1>
+        <span className="span-home">Cole seu link para encurtar 👇</span>
+
+        <div className="area-input">
+          <div className="input-home">
+            <FiLink className="icon-home" size={24} color="#fff" />
+            <input
+              className="input-this"
+              type="text"
+              placeholder="Cole seu link aqui"
+            />
+          </div>
+
+          <button>Gerar link</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/routes/RoutesApp.js
+++ src/routes/RoutesApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Links from '../pages/Links/Links';
 
